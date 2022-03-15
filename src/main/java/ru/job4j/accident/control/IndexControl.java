7a216e5b7b1e49c6bebd3ac5d049ed0cee0b9ecc
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-03-16 00:18:16 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> list = new ArrayList<>();
+        list.add("Vasya");
+        list.add("Masha");
+        list.add("Nikita");
+        model.addAttribute("names", list);
+        model.addAttribute("user", "Мой юзер");
         return "index";
     }
 }
